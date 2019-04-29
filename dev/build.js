a76f7bd544d123fe20b8ec3dc5ca245f6cbf9b9e
petulla <sam.petulla@gmail.com>
2019-04-29 18:53:22 -0400

    Add ds portfolio I

--- dev/build.js
+++ dev/build.js
@@ -6,13 +6,15 @@ const ncp = require('ncp');
 const config = {};
 config.src = './src';
 config.build = './build';
-config.ignore = './src/__helpers/**';
+config.ignore = './__helpers/**';
 
 const outputpath = {};
 outputpath.source = config.build + '/index.html';
 outputpath.destination = '../index.html';
 
 const logger = err => console.log(err);
+
+/* ./index.html */
 
 metalsmith(__dirname)
   .source(config.src)
@@ -30,6 +32,41 @@ metalsmith(__dirname)
   .build((err) => {
     if (err) logger(err);
     ncp(outputpath.source, outputpath.destination, (errcopy) => {
+      if (errcopy) {
+        return console.error('copying ' + err);
+      }
+      console.log('Done!');
+      return null;
+    });
+  });
+
+/* ds microsite */
+
+const dsConfig = {};
+dsConfig.src = './srcDS';
+dsConfig.build = './buildDS';
+dsConfig.ignore = './__helpers/**';
+
+const dsOutputpath = {};
+dsOutputpath.source = dsConfig.build + '/index.html';
+dsOutputpath.destination = '../ds/index.html';
+
+metalsmith(__dirname)
+  .source(dsConfig.src)
+  .ignore([dsConfig.ignore])
+  .destination(dsConfig.build)
+  .use(metadata({
+    meta: 'data/meta.yaml',
+    analyses: 'data/analyses.yaml',
+    explorables: 'data/explorables.yaml',
+    articles: 'data/articles.yaml'
+  }))
+  .use(layouts({
+    directory: './layoutsDS', default: 'index.hbs'
+  }))
+  .build((err) => {
+    if (err) logger(err);
+    ncp(dsOutputpath.source, dsOutputpath.destination, (errcopy) => {
       if (errcopy) {
         return console.error('copying ' + err);
       }
